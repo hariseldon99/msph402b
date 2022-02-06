f2fab098c9a4370dadd052b54b923ac61918c137
Analabha Roy <daneel@utexas.edu>
2022-02-06 20:37:55 +0530

    started teaching repository for msph402b with mprecision and interpolation codes

---
+++ 02-Interpolation/runge_interp.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  6 16:00:47 2022
+
+@author: Analabha Roy
+"""
+
+import numpy as np
+from scipy.interpolate import lagrange
+import matplotlib.pyplot as plt
+
+def runge(x):
+    return 1/(1 + 25 * x**2)
+
+x = np.arange(-1., 1., 0.01)
+plt.plot(x, runge(x), label = 'Exact function')
+
+orders = [2,5,10,19]
+
+for order in orders:
+    xvals = np.linspace(-1.0, 1.0, order + 1)
+    interpolant = lagrange(xvals, runge(xvals))
+    plt.plot(x, np.polyval(interpolant, x), label="order %d" % (order), alpha=0.6)    
+
+plt.ylim(-0.9,1.45)
+plt.legend()
+plt.show()
---
+++ README.md.old
@@ -0,0 +1,2 @@
+# msph402b
+Python codes for MSPH402B - Computational Physics, taught at the Department of Physics, The University of Burdwan
